5ebd803872317b31a8e865202ea95c12c9631d6f
Bruno <brunofnz28@gmail.com>
2023-03-04 16:12:27 -0300

    add signMessage

---
+++ test/recoverKey.test.js
@@ -0,0 +1,21 @@
+const signMessage = require('../chapter01/signMessage');
+const recover = require('../chapter01/recoverKey');
+const secp = require("ethereum-cryptography/secp256k1");
+const { assert } = require('chai');
+const { toHex } = require("ethereum-cryptography/utils");
+
+// const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+
+const { PRIVATE_KEY } = process.env
+
+describe('Recover Key', () => {
+    it('should recover the public key from a signed message', async () => {
+        const [sig, recoveryBit] = await signMessage('hello world');
+
+        const publicKey = secp.getPublicKey(PRIVATE_KEY);
+
+        const recovered = await recover('hello world', sig, recoveryBit);
+
+        assert.equal(toHex(recovered), toHex(publicKey));
+    });
+});
